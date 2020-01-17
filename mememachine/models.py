8ba49465736d204d92c3eb21dc8370879d457fc5
Kian Chakamian <kian.chakamian@gmail.com>
2020-01-17 15:56:44 -0800

    Creation of signup class

---
+++ mememachine/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length = 50)
+    email = models.CharField()
+    password = models.CharField(max_length = 50)
+    pfp = models.CharField()
+    liked_posts = models
--- mememachine/urls.py
+++ mememachine/urls.py
@@ -18,4 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^signup/', views.signup, name='signup')
 ]
